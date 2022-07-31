b12fa8e58567eb25c29d73b85dc41bf1ae5dfbe1
Shank-001 <patilshashank929@gmail.com>
2022-07-31 22:04:19 +0530

    Sunday commit

--- login.rb
+++ login.rb
@@ -5,6 +5,8 @@ require 'user'
 module Login
   @@admin_name = 'Shank'
   @@admin_password = 'Pro@123'
+  include Admin
+  include User
 
   def login
     puts "\n------Login------"
@@ -13,72 +15,34 @@ module Login
     print 'Enter Password: '
     password = gets.chomp
     puts
-
+    
     if username == @@admin_name && password == @@admin_password
       # Proceed to Admin Dashboard
-      include Admin
-      Admin.show
-    elsif username == @@username && password == @@password
+      Main.new.show
+    elsif username == @username && password == @password
       # Proceed to User Dashboard
-      include User
-      User.show
+      Main.new.show
     else
       puts 'Authentication Failed! Try again.'
       login
     end
   end
-
-  # def admin_login
-  #   puts "\n------Login------"
-  #   print 'Enter Admin name: '
-  #   username = gets.chomp
-  #   print 'Enter Admin password: '
-  #   password = gets.chomp
-  #   puts
-
-  #   if username == @@admin_name && password == @@admin_password
-  #     # Proceed to Admin Dashboard
-  #     # include Admin
-  #     require 'admin'
-  #   else
-  #     puts "Authentication Failed! Try again."
-  #     admin_login
-  #   end
-  # end
-
+  
   def signup
     puts "\n------Signup------"
     print 'Enter Username: '
-    @@username = gets.chomp
+    @username = gets.chomp
     print 'Enter Password: '
-    @@password = gets.chomp
+    @password = gets.chomp
     puts 'Successfully Signed Up.'
     # Calling user_login method
     login
   end
 
-  # def user_login
-  #   puts "\n------Login------"
-  #   print 'Enter Username: '
-  #   username = gets.chomp
-  #   print 'Enter Password: '
-  #   password = gets.chomp
-  #   puts
-
-  #   if username == @username && password == @password
-  #     # Proceed to User Dashboard
-  #     # include User
-  #     require 'user'
-  #   else
-  #     puts 'Authentication Failed! Try again.'
-  #     user_login
-  #   end
-  # end
-
   # Checking type of user.
   def check_user
     puts 'Signup/Login to continue.'
-    print "Enter 'a' for Admin or 'b' for User: "
+    print "\nEnter 'a' for Admin or 'b' for User: "
     user_type = gets.chomp
 
     if user_type == 'a'
--- user.rb
+++ user.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH << '.'
 require 'product_manage'
+include ProductManage
 
 module User
   def show
     puts '------User Dashboard------'
+    Main.new.show_item_list
   end
 end
